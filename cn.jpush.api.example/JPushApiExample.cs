efa55f96559c56243f76161d40b45cb0f8739da8
wawa0210 <xiaozhang0210@hotmail.com>
2015-09-02 11:06:35 +0800

    新建之后的第一次保存 2015-09-02 11:06

---
+++ cn.jpush.api.example/JPushApiExample.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+using cn.jpush.api;
+using cn.jpush.api.push;
+using cn.jpush.api.report;
+using cn.jpush.api.common;
+using cn.jpush.api.util;
+using cn.jpush.api.push.mode;
+using cn.jpush.api.push.notification;
+using cn.jpush.api.common.resp;
+namespace JpushApiClientExample
+{
+    class JPushApiExample
+    {
+        public static String TITLE = "Test from C# v3 sdk";
+        public static String ALERT = "Test from  C# v3 sdk - alert";
+        public static String MSG_CONTENT = "Test from C# v3 sdk - msgContent";
+        public static String REGISTRATION_ID = "0900e8d85ef";
+        public static String TAG = "tag_api";
+        public static String app_key = "997f28c1cea5a9f17d82079a";
+        public static String master_secret = "47d264a3c02a6a5a4a256a45";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****开始发送******");
+            JPushClient client = new JPushClient(app_key, master_secret);
+            PushPayload payload = PushObject_All_All_Alert();
+            try
+            {
+                var result = client.SendPush(payload);
+                //由于统计数据并非非是即时的,所以等待一小段时间再执行下面的获取结果方法
+                System.Threading.Thread.Sleep(10000);
+                /*如需查询上次推送结果执行下面的代码*/
+                var apiResult = client.getReceivedApi(result.msg_id.ToString());
+                var apiResultv3 = client.getReceivedApi_v3(result.msg_id.ToString());
+                /*如需查询某个messageid的推送结果执行下面的代码*/
+                var queryResultWithV2 = client.getReceivedApi("1739302794"); 
+                var querResultWithV3 = client.getReceivedApi_v3("1739302794");
+
+            }
+            catch (APIRequestException e)
+            {
+                Console.WriteLine("Error response from JPush server. Should review and fix it. ");
+                Console.WriteLine("HTTP Status: " + e.Status);
+                Console.WriteLine("Error Code: " + e.ErrorCode);
+                Console.WriteLine("Error Message: " + e.ErrorCode);
+            }
+            catch (APIConnectionException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("*****结束发送******");
+        }
+        public static PushPayload PushObject_All_All_Alert()
+        {
+            PushPayload pushPayload = new PushPayload();
+            pushPayload.platform = Platform.all();
+            pushPayload.audience = Audience.all();
+            pushPayload.notification = new Notification().setAlert(ALERT);
+            return pushPayload;
+        }
+        public static PushPayload PushObject_all_alias_alert()
+        {
+
+            PushPayload pushPayload = new PushPayload();
+            pushPayload.platform = Platform.android();
+            pushPayload.audience = Audience.s_alias("alias1");
+            pushPayload.notification = new Notification().setAlert(ALERT);
+            return pushPayload;
+           
+        }
+        public static PushPayload PushObject_Android_Tag_AlertWithTitle()
+        {
+            PushPayload pushPayload = new PushPayload();
+
+            pushPayload.platform = Platform.android();
+            pushPayload.audience = Audience.s_tag("tag1"); 
+            pushPayload.notification =  Notification.android(ALERT,TITLE);
+
+            return pushPayload;
+        }
+        public static PushPayload PushObject_android_and_ios()
+        {
+            PushPayload pushPayload = new PushPayload();
+            pushPayload.platform = Platform.android_ios();
+            var audience = Audience.s_tag("tag1");
+            pushPayload.audience = audience;
+            var notification = new Notification().setAlert("alert content");
+            notification.AndroidNotification = new AndroidNotification().setTitle("Android Title");
+            notification.IosNotification = new IosNotification();
+            notification.IosNotification.incrBadge(1);
+            notification.IosNotification.AddExtra("extra_key", "extra_value");
+
+            pushPayload.notification = notification.Check(); 
+      
+
+            return pushPayload;
+        }
+        public static PushPayload PushObject_ios_tagAnd_alertWithExtrasAndMessage()
+        {
+            PushPayload pushPayload = new PushPayload();
+            pushPayload.platform = Platform.android_ios();
+            pushPayload.audience = Audience.s_tag_and("tag1", "tag_all");
+            var notification = new Notification();
+            notification.IosNotification = new IosNotification().setAlert(ALERT).setBadge(5).setSound("happy").AddExtra("from","JPush");
+
+            pushPayload.notification = notification;
+            pushPayload.message = Message.content(MSG_CONTENT);
+            return pushPayload;
+
+        }
+        public static PushPayload PushObject_ios_audienceMore_messageWithExtras()
+        {
+            
+            var pushPayload = new PushPayload();
+            pushPayload.platform = Platform.android_ios();
+            pushPayload.audience = Audience.s_tag("tag1","tag2");
+            pushPayload.message = Message.content(MSG_CONTENT).AddExtras("from", "JPush");
+            return pushPayload;
+
+        }
+
+    }
+}
---
+++ cn.jpush.api.example/ReportsExample.cs
@@ -0,0 +1,84 @@
+﻿using cn.jpush.api.common;
+using cn.jpush.api.common.resp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cn.jpush.api.example
+{
+    class ReportsExample
+    {
+         public static String app_key = "997f28c1cea5a9f17d82079a";
+         public static String master_secret = "47d264a3c02a6a5a4a256a45";
+
+         public static void testGetReport()
+         {
+             JPushClient jpushClient = new JPushClient(app_key, master_secret);
+             try
+             {
+                 var result = jpushClient.getReceivedApi("1942377665");
+                 Console.WriteLine("Got result - " + result.ToString());
+
+             }
+             catch (APIRequestException e)
+             {
+                 Console.WriteLine("Error response from JPush server. Should review and fix it. ");
+                 Console.WriteLine("HTTP Status: " + e.Status);
+                 Console.WriteLine("Error Code: " + e.ErrorCode);
+                 Console.WriteLine("Error Message: " + e.ErrorCode);
+             }
+             catch (APIConnectionException e)
+             {
+                 Console.WriteLine(e.Message);
+             }
+         }
+         /*用户统计vip专用接口*/
+         public static void testGetUsers()
+         {
+             JPushClient jpushClient = new JPushClient(app_key, master_secret);
+             try
+             {
+                 var result = jpushClient.getReportUsers(TimeUnit.DAY, "2014-06-10", 3);
+                 Console.WriteLine("Got result - " + result.ToString());
+
+             }
+             catch (APIRequestException e)
+             {
+                 Console.WriteLine("Error response from JPush server. Should review and fix it. ");
+                 Console.WriteLine("HTTP Status: " + e.Status);
+                 Console.WriteLine("Error Code: " + e.ErrorCode);
+                 Console.WriteLine("Error Message: " + e.ErrorCode);
+             }
+             catch (APIConnectionException e)
+             {
+                 Console.WriteLine(e.Message);
+             }
+         }
+         /*消息统计vip专用接口*/
+         public static void testGetMessages()
+         {
+             JPushClient jpushClient = new JPushClient(app_key, master_secret);
+             try
+             {
+                 var result = jpushClient.getReportMessages("269978303");
+                 Console.WriteLine("Got result - " + result.ToString());
+
+             }
+             catch (APIRequestException e)
+             {
+                 Console.WriteLine("Error response from JPush server. Should review and fix it. ");
+                 Console.WriteLine("HTTP Status: " + e.Status);
+                 Console.WriteLine("Error Code: " + e.ErrorCode);
+                 Console.WriteLine("Error Message: " + e.ErrorCode);
+             }
+             catch (APIConnectionException e)
+             {
+                 Console.WriteLine(e.Message);
+             }
+
+         }
+
+    }
+}
